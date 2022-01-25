3ed6e39676a7e640253d288514a3c885173247f8
kaká cordovil <javanaoecafe@gmail.com>
2022-01-25 18:58:30 +0000

    created php page

---
+++ internet_module/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>My first PHP projetc on Dio</title>
+    </head>
+
+    <body>
+        <?php
+            for ($i=0; $i < 10; $i++) { 
+                print("Number line " . $i . "<br/>");
+            }
+        
+        ?>
+    </body>
+</html>
