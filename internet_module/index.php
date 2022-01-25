5f742c0351c3658ed844f85b28a80100e9bf5531
kaká cordovil <javanaoecafe@gmail.com>
2022-01-25 19:16:03 +0000

    edited php page

--- internet_module/index.php
+++ internet_module/index.php
@@ -1,14 +1,28 @@
 <html>
     <head>
         <title>My first PHP projetc on Dio</title>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <style type="text/css">
+            .linha {
+                font-weight: bold;
+                color: pink;
+                padding-left: 10px;
+                line: height 50px;
+            }
+        </style>
     </head>
 
     <body>
         <?php
             for ($i=0; $i < 10; $i++) { 
-                print("Number line " . $i . "<br/>");
+                print("<span class=\"linha\">Number line " . $i . "</span><br/>");
             }
         
         ?>
     </body>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            alert("Alert!");            
+        });
+    </script>
 </html>
